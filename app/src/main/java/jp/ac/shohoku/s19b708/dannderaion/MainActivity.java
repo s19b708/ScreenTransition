c606e18761633bed0192af340e417c86ccb861e2
s19B708 <s19b708@shohoku.ac.jp>
2020-10-30 10:48:10 +0900

    カーカッカカー

--- app/src/main/java/jp/ac/shohoku/s19b708/dannderaion/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/s19b708/dannderaion/MainActivity.java
@@ -2,8 +2,6 @@ package jp.ac.shohoku.s19b708.dannderaion;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
